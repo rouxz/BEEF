b7cbca2e1981b932bc9196a462a7bb8521ff07fb
rouxz <rouxz@ymail.com>
2013-07-14 15:20:50 +0200

    14JUL13 Fin multiplatform

--- SRC/mainGui.py
+++ SRC/mainGui.py
@@ -18,6 +18,8 @@ class TopWindow(QMainWindow):
 	def __init__(self, core, platform = PLATFORM_WINDOWS):
 		# initiate the main widget
 		QMainWindow.__init__(self)
+		
+		self.platform = platform
 
 		#core
 		self.core = core
@@ -37,7 +39,7 @@ class TopWindow(QMainWindow):
 		# central widget
 		########################
 
-		self.setCentralWidget(CentralWidget(fm, core, self))
+		self.setCentralWidget(CentralWidget(fm, core, self, self.platform))
 
 		########################
 		# Status and menu bars
@@ -71,8 +73,10 @@ class TopWindow(QMainWindow):
 class CentralWidget(QWidget):
 	""" a widget being the main widget within the topwindow"""
 	""" this main widget will have all the required tabs and table within """
-	def __init__(self, fm, core, parent):
+	def __init__(self, fm, core, parent, platform = PLATFORM_WINDOWS):
 		QWidget.__init__(self, parent)
+		
+		self.platform = platform
 
 		self.initCentralWidget(fm, core)
 
@@ -101,7 +105,7 @@ class CentralWidget(QWidget):
 		self.grid.addWidget(self.title, 0 , 0, 1, -1)
 
 		# set the side panel
-		self.sidePanel = SidePanel(fm, core, self)
+		self.sidePanel = SidePanel(fm, core, self, self.platform)
 		self.grid.addWidget(self.sidePanel, 1, 0)
 
 		#setting the layout
